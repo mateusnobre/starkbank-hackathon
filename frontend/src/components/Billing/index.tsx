40c66507536748a1d51ca57f0d091523fc4a5c81
Bryan <bryanborck@gmail.com>
2023-05-25 17:38:27 -0300

    Billing first version done

--- frontend/src/components/Billing/index.tsx
+++ frontend/src/components/Billing/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 const Billingdiv = styled.div`
     display: flex;
@@ -15,8 +15,51 @@ const AuthForm = styled.form`
     display: flex;
     flex-direction: column;
     align-items: center;
-    height: 45%;
+    height: 42%;
     width: 100%;
+`;
+
+const PaymentForm = styled(AuthForm)`
+    height: 38%;
+`;
+
+const PaymentMethod = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 20%;
+    width: 100%;
+`;
+
+const MethodWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    height: calc(70% - 3vh);
+    width: 100%;
+
+    button {
+        height: 85%;
+        width: 31%;
+        background: transparent;
+        border: 1px solid var(--color-bkg-inv-soft);
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+        color: var(--color-bkg-inv-hard);
+        font-size: var(--fontsize);
+
+        background-image: linear-gradient(var(--color-bkg-soft), var(--color-bkg-soft) 50%, var(--color-bkg-inv-soft) 50%, var(--color-bkg-inv-soft));
+        background-size: 100% 210%;
+        transition: background-position .2s;
+
+        :hover{
+            cursor: pointer;
+            background-position: 0 100%;
+            color: var(--color-bkg-soft);
+        }
+    }
 `;
 
 const FormTitle = styled.div`
@@ -43,7 +86,7 @@ const InputName = styled.div`
     padding-bottom: .5vh;
 
     color: var(--color-bkg-inv-hard);
-    font-size: var(--fontsize-subtitle);
+    font-size: var(--fontsize-name);
     font-weight: 600;
 `;
 
@@ -55,7 +98,38 @@ const InputBox = styled.input`
     text-indent: 1vw;
     width: 99%;
     height: 5vh;
-    margin-bottom: 2vh;
+    margin-bottom: 1vh;
+
+    background: transparent;
+    border: 1px solid var(--color-bkg-inv-soft);
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    outline: 0;
+
+    color: var(--color-bkg-inv-hard);
+    font-size: var(--fontsize-input);
+    font-weight: 300;
+
+    ::placeholder {
+        color: var(--color-bkg-inv-soft);
+        opacity: 0.7;
+        font-weight: 300;
+    }
+
+    ::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+const SelectBox = styled.select`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    text-indent: 1vw;
+    width: 100%;
+    height: 5vh;
+    margin-bottom: 1vh;
 
     background: transparent;
     border: 1px solid var(--color-bkg-inv-soft);
@@ -82,7 +156,7 @@ const SendButton = styled.button`
 
     height: 6vh;
     width: 100%;
-    margin-top: 1vh;
+    margin-top: 1.5vh;
 
     outline: 0;
     border: 2px solid var(--color-pix);
@@ -90,7 +164,7 @@ const SendButton = styled.button`
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 
     color: var(--color-pix);
-    font-size: var(--fontsize-title);
+    font-size: var(--fontsize);
     font-weight: 600;
 
     background-image: linear-gradient(var(--color-bkg-soft), var(--color-bkg-soft) 50%, var(--color-pix) 50%, var(--color-pix));
@@ -105,19 +179,46 @@ const SendButton = styled.button`
 `;
 
 
-
 export default function Billing() {
 
     const [name, setName] = useState("");
 
-    const handleSubmit = () => {
+    const handleInfoSubmit = () => {
         console.log('form submitted ✅');
         alert(`The name you entered was: ${name}`)
     };
 
+    const handlePaymentSubmit = () => {
+        console.log('form submitted ✅');
+        alert(`The payment is done`)
+    };
+
+    const numberInputRef = useRef<HTMLInputElement>(null);
+
+
+    // Only to disable scrolling in input type number
+    useEffect(() => {
+        const numberInput = numberInputRef.current;
+
+        const handleWheel = (event: WheelEvent) => {
+        event.preventDefault();
+        };
+
+        if (numberInput) {
+        numberInput.addEventListener('wheel', handleWheel);
+        }
+
+        return () => {
+        if (numberInput) {
+            numberInput.removeEventListener('wheel', handleWheel);
+        }
+        };
+    }, []);
+    // Only to disable scrolling in input type number
+
     return (
         <Billingdiv>
-            <AuthForm>
+            <AuthForm onSubmit={handleInfoSubmit}>
                 <FormTitle>Personal Info</FormTitle>
                 <InputName>Email</InputName>
                 <InputBox 
@@ -131,10 +232,36 @@ export default function Billing() {
                 <InputBox 
                     type="number"
                     placeholder='XXXXX'
+                    ref={numberInputRef}
                 >
                 </InputBox>
                 <SendButton type="submit">Send Info</SendButton>
             </AuthForm>
+            <PaymentMethod>
+                <FormTitle>Payment Method</FormTitle>
+                <MethodWrapper>
+                    <button>PIX</button>
+                    <button>Credit Card</button>
+                    <button>Debt Card</button>
+                </MethodWrapper>
+            </PaymentMethod>
+            <PaymentForm onSubmit={handlePaymentSubmit}>
+                <InputName>Método</InputName>
+                <SelectBox 
+                    placeholder='Escolha em quantas vezes parcelar'
+                >
+                    <li>A vista</li>
+                    <li>1X</li>
+                    <li>2X</li>
+                </SelectBox>
+                <InputName>Chave Pix</InputName>
+                <InputBox 
+                    type="number"
+                    placeholder='XXXXX'
+                >
+                </InputBox>
+                <SendButton type="submit">Send Payment</SendButton>
+            </PaymentForm>
         </Billingdiv>
     );
 }
